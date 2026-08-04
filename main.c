f5e26ffaa10c4dddd8caa06b4bd0ce07b94f0813
Chad KS Shikuambi <chadshikuambi@gmail.com>
2026-08-04 21:02:48 +0200

    Week 1- First C Program

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    printf("Municipal Financial Management System\n");
+    printf("Welcome to Windhoek Municipality\n");
+
+    char municipality[50];
+    char mayor[50];
+    int population;
+    printf("Municipal Financial Management System\n\n");
+    printf("Enter Municipality Name: ");
+    scanf("%49s", municipality);
+
+    printf("Enter Mayor: ");
+    scanf("%49s", mayor);
+    printf("Enter Population: ");
+    scanf("%d", &population);
+    printf("\n---------------------------------\n");
+    printf("Municipality : %s\n", municipality);
+    printf("Mayor : %s\n", mayor);
+    printf("Population : %d\n", population);
+
+
+    return 0;
+}
